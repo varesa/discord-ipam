7de06395b112bb9441cfdb67fdc0b6d6f274e2d9
Esa Varemo <git@esav.fi>
2023-06-04 23:55:58 +0300

    Move HTML to separate template

--- ipam/app.py
+++ ipam/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, render_template
 from flask_discord import requires_authorization
 
 from config import config_app
@@ -16,20 +16,12 @@ def root():
     user = discord.fetch_user()
     my_ASNs, other_ASNs = get_user_ASNs(user)
 
-    my_ASNs_str = "<br>".join([ASN.display() for ASN in my_ASNs])
-    other_ASNs_str = "<br>".join([ASN.display() for ASN in other_ASNs])
+    context = {
+        "my_ASNs_str": "<br>".join([ASN.display() for ASN in my_ASNs]),
+        "other_ASNs_str": "<br>".join([ASN.display() for ASN in other_ASNs]),
+    }
 
-    return f"""
-    <html>
-        <head>
-            <title>Srvlab IPAM test</title>
-        </head>
-        <body>
-            <p>User info: Authenticated as {user.id} ({user})</p>
-            <p>My ASNs:<br>{my_ASNs_str}</p>
-            <p>Other ASNs:<br>{other_ASNs_str}</p>
-        </body>
-    </html>"""
+    return render_template("home.html", **context)
 
 
 if __name__ == "__main__":
